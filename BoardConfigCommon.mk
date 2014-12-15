bb0a881089dc79f662e2c44b3d5fe7e37fff4f70
bryan2894 <bryan2894@gmail.com>
2014-12-15 11:55:30 -0500

    g3: add sepolicy rule for IrRC
    Change-Id: I6881ad51743aecadfaa75621cc59a83acb03485f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,7 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    app.te \
     device.te \
     file.te \
     file_contexts \
@@ -152,6 +153,7 @@ BOARD_SEPOLICY_UNION += \
     property_contexts \
     radio.te \
     rmt_storage.te \
+    seapp_contexts \
     sensors.te \
     servicemanager.te \
     sysinit.te \
